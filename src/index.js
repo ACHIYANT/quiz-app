d72128c13fe85a44a0bf04a19d80f9c5bc10888e
ACHIYANT <achiyant@gmail.com>
2023-11-13 22:48:53 +0530

    Necessary changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Appcopy from './components/App';
+const style = 'background-color: red; color: white; font-style: italic; border: 5px solid black; font-size: 4em; text-align:center;'
+console.log("%c!WARNING", style);
+const style1 = 'background-color: red; color:white; font-style: bold; font-size:4em;'
+console.error("%cDon't type anything in console. It will auto submit your quiz.",style1);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Appcopy />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
